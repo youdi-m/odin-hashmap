5a941981c153648e7f7e3cf1d034dcf2d199a70b
youdi <mehdi.yousef97@gmail.com>
2026-04-30 22:05:58 -0400

    got main functions in

---
+++ hashmap.js
@@ -0,0 +1,47 @@
+export class HashMap {
+	constructor() {
+		this.capacity = 16;
+		this.loadFactor = 0.75;
+	}
+
+	// takes a key and produces a hash code with it
+	hash(key) {
+
+	}
+
+	set(key, value) {
+
+	}
+
+	get(key) {
+
+	}
+
+	has(key) {
+
+	}
+
+	remove(key) {
+
+	}
+
+	length() {
+
+	}
+
+	clear() {
+
+	}
+
+	keys() {
+
+	}
+
+	values() {
+
+	}
+
+	entries() {
+
+	}
+}
